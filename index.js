12499be9e8d0d9a69d44b12e030059edfacce243
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-18 14:25:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const thankYou = ['July','Maggie','Meg']
+
+function writeCards(name, event){
+    let msg = []
+    for (let i = 0; i<thankYou.length; i++){
+        msg.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return msg;
+
+}
+function countDown(num){
+    
+    while (num > -1){
+        console.log (num--);
+    }
+}
